d352d4205cf3927eb5fc28ee60958548c6732354
Daniel Manila <dmv@springwater7.org>
2016-11-22 16:20:03 +0600

    Moves GUI method over into class

--- src/weresync/gui.py
+++ src/weresync/gui.py
@@ -41,62 +41,66 @@ def create_help_box(parent, text, title=""):
     help.connect("activate-link", help_click)
     return help
 
+class WereSyncWindow(Gtk.Window):
+    def __init__(self, title="WereSync"):
+        super().__init__(title=title)
+        self.grid = Gtk.Grid()
+        self.add(self.grid)
+        self.source_label = Gtk.Label(label="Source Drive: ", halign=Gtk.Align.START, xpad=DEFAULT_HORIZONTAL_PADDING, ypad=DEFAULT_VERTICAL_PADDING)
+        self.source_label.set_hexpand(False)
+        #TODO add actually drive names
+        name_store = Gtk.ListStore(int, str)
+        name_store.append([1, "/dev/sda"])
+        name_store.append([2, "/dev/sdb"])
+        self.source_combo = Gtk.ComboBox.new_with_model_and_entry(name_store)
+        self.source_combo.set_hexpand(True)
+        self.grid.attach(self.source_label, 1, 1, 1, 1)
+        self.grid.attach_next_to(self.source_combo, self.source_label, Gtk.PositionType.RIGHT, 1, 1)
+        self.target_label = Gtk.Label(label="Target Drive: ", halign=Gtk.Align.START, xpad=DEFAULT_HORIZONTAL_PADDING, ypad=DEFAULT_VERTICAL_PADDING)
+        self.target_combo = Gtk.ComboBox.new_with_model_and_entry(name_store)
+        self.target_combo.set_hexpand(True)
+        self.grid.attach_next_to(self.target_label, self.source_label, Gtk.PositionType.BOTTOM, 1, 1)
+        self.grid.attach_next_to(self.target_combo, self.target_label, Gtk.PositionType.RIGHT, 1, 1)
+        self.copy_partitions_button = Gtk.CheckButton(label="Copy partitions if target partitions are invalid.")
+        set_margin(self.copy_partitions_button)
+        self.grid.attach_next_to(self.copy_partitions_button, self.target_label, Gtk.PositionType.BOTTOM, 2, 1)
+        self.efi_partition_label = Gtk.Label(label="EFI Partition Number: ", halign=Gtk.Align.START, xpad=DEFAULT_HORIZONTAL_PADDING, ypad=DEFAULT_VERTICAL_PADDING)
+        self.grid.attach_next_to(self.efi_partition_label, self.copy_partitions_button,
+                            Gtk.PositionType.BOTTOM, 1, 1)
+        self.efi_partition_entry = NumberEntry()
+        self.efi_partition_entry.set_hexpand(True)
+        self.grid.attach_next_to(self.efi_partition_entry, self.efi_partition_label, Gtk.PositionType.RIGHT, 1, 1)
+        self.efi_help = create_help_box(self, "Enter the partition number of your EFI partition.\n"
+                                   "So if your efi partition is found on /dev/sda1,"
+                                   " enter 1.",
+                                   "EFI Partition")
+        self.grid.attach_next_to(self.efi_help, self.efi_partition_entry, Gtk.PositionType.RIGHT, 1, 1)
+        self.boot_partition_label = Gtk.Label(label="Boot Partition Number: ", halign=Gtk.Align.START, xpad=DEFAULT_HORIZONTAL_PADDING, ypad=DEFAULT_VERTICAL_PADDING)
+        self.grid.attach_next_to(self.boot_partition_label, self.efi_partition_label, Gtk.PositionType.BOTTOM,
+                            1, 1)
+        self.boot_partition_entry = NumberEntry()
+        self.grid.attach_next_to(self.boot_partition_entry, self.boot_partition_label, Gtk.PositionType.RIGHT, 1, 1)
+        self.boot_help = create_help_box(self, "Enter the partition number of the partition"
+                                    " to install the bootloader on. This is generally the partition mounted on /\n"
+                                    "So if your root directory is /dev/sda2, enter 2.",
+                                    "Boot Partition")
+        self.grid.attach_next_to(self.boot_help, self.boot_partition_entry, Gtk.PositionType.RIGHT, 1, 1)
+        self.expander = Gtk.Expander(label="Advanced Options")
+        set_margin(self.expander)
+        self.expander.set_resize_toplevel(True)
+        self.expand_grid = Gtk.Grid()
+        self.expander.add(self.expand_grid)
+        test = Gtk.Label(label="test")
+        self.expand_grid.add(test)
+        self.expander.set_hexpand(True)
+        self.grid.attach_next_to(self.expander, self.boot_partition_label, Gtk.PositionType.BOTTOM, 1, 1)
+        self.start = Gtk.Button(label="Start Clone")
+        set_margin(self.start)
+        self.start.set_hexpand(False)
+        self.grid.attach(self.start, 3, 10, 1, 1)
+
 def start_gui():
-    win = Gtk.Window(title="WereSync")
+    win = WereSyncWindow()
     win.connect("delete-event", Gtk.main_quit)
-    grid = Gtk.Grid()
-    win.add(grid)
-    source_label = Gtk.Label(label="Source Drive: ", halign=Gtk.Align.START, xpad=DEFAULT_HORIZONTAL_PADDING, ypad=DEFAULT_VERTICAL_PADDING)
-    source_label.set_hexpand(False)
-    #TODO add actually drive names
-    name_store = Gtk.ListStore(int, str)
-    name_store.append([1, "/dev/sda"])
-    name_store.append([2, "/dev/sdb"])
-    source_combo = Gtk.ComboBox.new_with_model_and_entry(name_store)
-    source_combo.set_hexpand(True)
-    grid.attach(source_label, 1, 1, 1, 1)
-    grid.attach_next_to(source_combo, source_label, Gtk.PositionType.RIGHT, 1, 1)
-    target_label = Gtk.Label(label="Target Drive: ", halign=Gtk.Align.START, xpad=DEFAULT_HORIZONTAL_PADDING, ypad=DEFAULT_VERTICAL_PADDING)
-    target_combo = Gtk.ComboBox.new_with_model_and_entry(name_store)
-    target_combo.set_hexpand(True)
-    grid.attach_next_to(target_label, source_label, Gtk.PositionType.BOTTOM, 1, 1)
-    grid.attach_next_to(target_combo, target_label, Gtk.PositionType.RIGHT, 1, 1)
-    copy_partitions_button = Gtk.CheckButton(label="Copy partitions if target partitions are invalid.")
-    set_margin(copy_partitions_button)
-    grid.attach_next_to(copy_partitions_button, target_label, Gtk.PositionType.BOTTOM, 2, 1)
-    efi_partition_label = Gtk.Label(label="EFI Partition Number: ", halign=Gtk.Align.START, xpad=DEFAULT_HORIZONTAL_PADDING, ypad=DEFAULT_VERTICAL_PADDING)
-    grid.attach_next_to(efi_partition_label, copy_partitions_button,
-                        Gtk.PositionType.BOTTOM, 1, 1)
-    efi_partition_entry = NumberEntry()
-    efi_partition_entry.set_hexpand(True)
-    grid.attach_next_to(efi_partition_entry, efi_partition_label, Gtk.PositionType.RIGHT, 1, 1)
-    efi_help = create_help_box(win, "Enter the partition number of your EFI partition.\n"
-                               "So if your efi partition is found on /dev/sda1,"
-                               " enter 1.",
-                               "EFI Partition")
-    grid.attach_next_to(efi_help, efi_partition_entry, Gtk.PositionType.RIGHT, 1, 1)
-    boot_partition_label = Gtk.Label(label="Boot Partition Number: ", halign=Gtk.Align.START, xpad=DEFAULT_HORIZONTAL_PADDING, ypad=DEFAULT_VERTICAL_PADDING)
-    grid.attach_next_to(boot_partition_label, efi_partition_label, Gtk.PositionType.BOTTOM,
-                        1, 1)
-    boot_partition_entry = NumberEntry()
-    grid.attach_next_to(boot_partition_entry, boot_partition_label, Gtk.PositionType.RIGHT, 1, 1)
-    boot_help = create_help_box(win, "Enter the partition number of the partition"
-                                " to install the bootloader on. This is generally the partition mounted on /\n"
-                                "So if your root directory is /dev/sda2, enter 2.",
-                                "Boot Partition")
-    grid.attach_next_to(boot_help, boot_partition_entry, Gtk.PositionType.RIGHT, 1, 1)
-    expander = Gtk.Expander(label="Advanced Options")
-    set_margin(expander)
-    expander.set_resize_toplevel(True)
-    expand_grid = Gtk.Grid()
-    expander.add(expand_grid)
-    test = Gtk.Label(label="test")
-    expand_grid.add(test)
-    expander.set_hexpand(True)
-    grid.attach_next_to(expander, boot_partition_label, Gtk.PositionType.BOTTOM, 1, 1)
-    start = Gtk.Button(label="Start Clone")
-    set_margin(start)
-    start.set_hexpand(False)
-    grid.attach(start, 3, 10, 1, 1)
     win.show_all()
     Gtk.main()
